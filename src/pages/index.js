6dde355744c1fa280807a2134aaa3a2ffe716ee3
jakir442 <jakigeming2021@gmail.com>
2024-01-17 21:55:41 +0700

    Portofolio NextJS

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
       <main className="flex items-center text-dark w-full min-h-screen mb-16 dark:text-light">
         <Layout className="pt-0 md:pt-16 sm:pt-8">
           <div className="flex items-center justify-between w-full lg:flex-col">
-            <div className="w-1/2 md:w-full mr-8">
+            <div className="w-1/2 md:w-full ">
               <Image
                 src={profilePic}
                 alt="Hi Kids"
@@ -31,11 +31,15 @@ export default function Home() {
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
               />
             </div>
-            <div className="w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center">
+            <div
+              className="w-1/2 flex flex-col items-center self-center lg:w-full 
+            lg:text-center ml-8"
+            >
               <h1></h1>
               <AnimatedText
                 text="Turning Vision Into Reality With Code And Design."
-                className="!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
+                className="!text-6xl !text-left xl:!text-5xl 
+                lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
               />
               <p className="my-4 text-base font-medium md:text-sm sm:text-xs">
                 {" "}
